a419d9c5dad6e177275301a9de698f2d54e0f341
Kittipat <iam_max21@hotmail.com>
2024-02-29 00:11:01 +0700

    🍱 add image assets

---
+++ src/assets/images.ts
@@ -0,0 +1,8 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+
+const images = {
+  americanfootball: "./png/americanfootball.png",
+  basketball: "./png/basketball.png",
+};
+
+export default images;
