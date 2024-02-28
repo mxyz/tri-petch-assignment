744afc90d9fce177eaad259b7a308d751521136a
Kittipat <iam_max21@hotmail.com>
2024-02-29 03:35:18 +0700

    🍱 update images code

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -1,8 +1,11 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 
+import americanfootballImage from "./png/americanfootball.png";
+import basketballImage from "./png/basketball.png";
+
 const images = {
-  americanfootball: "./png/americanfootball.png",
-  basketball: "./png/basketball.png",
+  americanfootball: americanfootballImage,
+  basketball: basketballImage,
 };
 
 export default images;
